a2da123804cf96d9f7cf467e3be7b2ac17f8edb9
samanritgithub <ssamankumari6@gmail.com>
2023-07-22 23:41:00 +0530

    customer details frame add

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ root.iconbitmap("icon.ico")
 headingLabel = Label(root, text="Billing System",font=('times new roman',30, 'bold'), bg='pink3',fg='maroon4',bd=12,relief=GROOVE )
 headingLabel.pack(fill=X,pady=10)
 
+customer_details_frame=LabelFrame(root, text='Customer Details',font=('times new roman',15,'bold'),fg='pink3',bd=8,relief=GROOVE,bg='gray24')
+customer_details_frame.pack(fill=X)
+
 
 
 
