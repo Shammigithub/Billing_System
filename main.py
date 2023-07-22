8f45182ee170157b8f941a7dc2662ae37ad05216
samanritgithub <ssamankumari6@gmail.com>
2023-07-22 23:40:14 +0530

    heading label add

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ root.title("Billing System")
 root.geometry("1190x758")
 root.iconbitmap("icon.ico")
 
+headingLabel = Label(root, text="Billing System",font=('times new roman',30, 'bold'), bg='pink3',fg='maroon4',bd=12,relief=GROOVE )
+headingLabel.pack(fill=X,pady=10)
+
 
 
 
