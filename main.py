c815e1ca10f05d95d65810fd4d65bbce0a53248c
Udumalagala Gamage Shammi Saman Kumari <99946678+Shammigithub@users.noreply.github.com>
2023-07-23 11:12:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -287,6 +287,7 @@ def clear():
 root = Tk()
 root.title("Billing System")
 root.geometry("1200x758")
+root.resizable(0,0)
 root.iconbitmap("icon.ico")
 
 headingLabel = Label(root, text="Billing System",font=('times new roman',30, 'bold'), bg='pink3',fg='maroon4',bd=12,relief=GROOVE )
